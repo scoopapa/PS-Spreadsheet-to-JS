5c8ee5c6a2d4dab5e90ad3439f21cbf6e0c8649e
scoopapa <pccompositor@aim.com>
2025-12-15 13:23:15 -0500

    fix single entry

--- js/parse.js
+++ js/parse.js
@@ -155,7 +155,6 @@
 		},
 	};
 	var parseDexColumn = function(key, ids) {
-		console.log("key: " + key + "| ids: " + ids);
 		if (!data.inputData[key]) return;
 		var arr = data.inputData[key].split('\n'); // separate each input into an array by newline char, then parse each element individually
 		var obj = {};
